a1c027c5358f88e723b747b4aaf63554edb9b33b
norihisa1003 <norihisa1003@shikon.meiji.ac.jp>
2019-05-25 23:16:20 +0900

    ドローンからのレスポンスを表示する処理を追加

--- program/tools/drone_manager.py
+++ program/tools/drone_manager.py
@@ -1,6 +1,7 @@
 import logging
 import socket
 import sys
+import threading
 import time
 
 
@@ -18,24 +19,62 @@ class DroneManager(object):
         self.drone_address = (drone_ip, drone_port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.host_ip, self.host_port))
-        self.socket.sendto(b'command', self.drone_address)
-        self.socket.sendto(b'streamon', self.drone_address)
+
+        self.response = None
+        self.stop_event = threading.Event()
+        self._response_thread = threading.Thread(target=self.receive_response,
+                                                 args=(self.stop_event, ))
+        self._response_thread.start()
+
+        self.send_command('command')
+        self.send_command('streamon')
+
+    def receive_response(self, stop_event):
+        while not stop_event.is_set():
+            try:
+                self.response, ip = self.socket.recvfrom(3000)
+                logger.info({'action': 'receive_response',
+                             'response': self.response})
+            except socket.error as ex:
+                logger.error(({'action': 'receive_response',
+                               'ex': ex}))
+                break
 
     def __dell__(self):
         self.stop()
 
     def stop(self):
+        self.stop_event.set()
+        retry = 0
+        while self._response_thread.isAlive():
+            time.sleep(0.3)
+            if retry > 30:
+                break
+            retry += 1
         self.socket.close()
 
     def send_command(self, command):
         logger.info({'action': 'send_command', 'command': command})
         self.socket.sendto(command.encode('utf-8'), self.drone_address)
 
+        retry = 0
+        while self.response is None:
+            time.sleep(0.3)
+            if retry > 3:
+                break
+            retry += 1
+        if self.response is None:
+            response = None
+        else:
+            response = self.response.decode('utf-8')
+        self.response = None
+        return response
+
     def takeoff(self):
-        self.send_command('takeoff')
+        return self.send_command('takeoff')
 
     def land(self):
-        self.send_command('land')
+        return self.send_command('land')
 
 
 if __name__ == '__main__':
