af5d0ebea4553fdd47ed365034922eb639154263
norihisa1003 <norihisa1003@shikon.meiji.ac.jp>
2019-06-09 16:26:36 +0900

    ドローンを開店する処理を追加

--- program/tools/drone_manager.py
+++ program/tools/drone_manager.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_DISTANCE = 0.30
 DEFAULT_SPEED = 10
+DEFAULT_DEGREE = 10
 
 
 class DroneManager(object):
@@ -111,28 +112,38 @@ class DroneManager(object):
     def set_speed(self, speed):
         return self.send_command(f'speed {speed}')
 
+    def clockwise(self, degree=DEFAULT_DEGREE):
+        return self.send_command(f'cw {degree}')
+
+    def counter_clockwise(self, degree=DEFAULT_DEGREE):
+        return self.send_command(f'ccw {degree}')
+
 
 if __name__ == '__main__':
     drone_manager = DroneManager()
 
-    drone_manager.set_speed(100)
-    drone_manager.takeoff()
+    # drone_manager.set_speed(100)
 
+    drone_manager.takeoff()
     time.sleep(10)
-    drone_manager.forward()
+    drone_manager.clockwise(90)
     time.sleep(5)
-    drone_manager.right()
+    drone_manager.counter_clockwise(90)
     time.sleep(5)
-    drone_manager.back()
-    time.sleep(5)
-    drone_manager.left()
-    time.sleep(5)
-    drone_manager.set_speed(10)
-    time.sleep(1)
-    drone_manager.up()
-    time.sleep(5)
-    drone_manager.down()
-    time.sleep(5)
+    # drone_manager.forward()
+    # time.sleep(5)
+    # drone_manager.right()
+    # time.sleep(5)
+    # drone_manager.set_speed(10)
+    # time.sleep(1)
+    # drone_manager.back()
+    # time.sleep(5)
+    # drone_manager.left()
+    # time.sleep(5)
+    # drone_manager.up()
+    # time.sleep(5)
+    # drone_manager.down()
+    # time.sleep(5)
 
     drone_manager.land()
     drone_manager.stop()
