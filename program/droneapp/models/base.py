b421ce18e024dafc27fefd1068792192a7a31485
norihisa1003 <norihisa1003@shikon.meiji.ac.jp>
2019-06-15 20:07:20 +0900

    Singletonパターンを追加

---
+++ program/droneapp/models/base.py
@@ -0,0 +1,10 @@
+class Singleton(type):
+
+    def __init__(cls, classname, superclases, attributes):
+        cls.instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls.instance is None:
+            print('call')
+            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
+            return cls.instance
---
+++ program/tools/single.py
@@ -0,0 +1,22 @@
+class Singleton(type):
+
+    def __init__(cls, classname, superclases, attributes):
+        cls.instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls.instance is None:
+            print('call')
+            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
+            return cls.instance
+
+
+class T(metaclass=Singleton):
+    def __init__(self):
+        print('init')
+
+
+test = T()
+test = T()
+test = T()
+test = T()
+test = T()
